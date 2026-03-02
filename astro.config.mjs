2b9a30ad81eaa36377eadf90f105542b6a3edfa6
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2026-03-02 09:21:45 +0000

    Fix images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://thedanikreal.github.io/",
-  base: "/YarixWeb"
+  base: "/YarixWeb",
+  build: {
+    format: "file"
+  }
 });
 
